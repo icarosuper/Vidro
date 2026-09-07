af37f5609dfd8bae69ebe647fa95858bbfd9bec2
icarosuper <icarogomesmotta@gmail.com>
2026-09-07 14:51:24 -0300

    fix: nao renderizar devtools fora de desenvolvimento

--- VidroFront/src/routes/__root.tsx
+++ VidroFront/src/routes/__root.tsx
@@ -78,13 +78,15 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-        <TanStackDevtools
-          config={{ position: 'bottom-right' }}
-          plugins={[
-            { name: 'Tanstack Router', render: <TanStackRouterDevtoolsPanel /> },
-            TanStackQueryDevtools,
-          ]}
-        />
+        {import.meta.env.DEV && (
+          <TanStackDevtools
+            config={{ position: 'bottom-right' }}
+            plugins={[
+              { name: 'Tanstack Router', render: <TanStackRouterDevtoolsPanel /> },
+              TanStackQueryDevtools,
+            ]}
+          />
+        )}
         <Scripts />
       </body>
     </html>
